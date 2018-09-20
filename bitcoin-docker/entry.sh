368f4d4502c5969138a0772f0262482d9684d37f
Steve Gehrman <sgehrman@gmail.com>
2018-09-20 11:30:12 -0700

    comment

--- bitcoin-docker/entry.sh
+++ bitcoin-docker/entry.sh
@@ -3,6 +3,8 @@
 # bitcoin.conf needed for using bitcoin-cli to on the server
 
 mkdir -p /root/.bitcoin
+
+# if file already exists, this appends and the first user/pass is used which is probably old and invalid (todo)
 [[ -n $RPCUSER ]] && echo "rpcuser=${RPCUSER}" >> /root/.bitcoin/bitcoin.conf
 [[ -n $RPCPASSWORD ]] && echo "rpcpassword=${RPCPASSWORD}" >> /root/.bitcoin/bitcoin.conf
 
